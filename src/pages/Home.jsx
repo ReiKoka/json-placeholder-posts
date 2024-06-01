ae3590be6bdc6a9545f62cf522bce89efa1c34be
ReiKoka <reikoka1@outlook.com>
2024-06-01 19:23:37 +0200

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 function Home() {
   return (
-    <div className="grid h-full place-items-center lg:grid-cols-2 grid-rows-2 ">
+    <div className="grid h-80% place-items-center lg:grid-cols-2 grid-rows-2">
       <img
         src="/logo/json-placeholder-posts-high-resolution-logo-transparent.svg"
         alt=""
-        className="my-6 w-[80%] rounded-3xl 2xs:w-[90%] xs:w-4/5 max-w-sm sm:max-w-md lg:row-start-1 lg:row-end-3 lg:w-[90%] lg:max-w-none lg:rounded-l-none lg:justify-items-start"
+        className="my-6 w-[50%] xs:w-4/5  max-w-sm sm:max-w-md lg:row-start-1 lg:row-end-3 lg:w-[80%] lg:max-w-none lg:rounded-l-none lg:justify-items-start"
       />
 
       <div className="flex flex-col items-center gap-6 text-center lg:justify-end lg:h-full ">
@@ -21,7 +21,7 @@ function Home() {
       </div>
 
       <div className="flex h-full w-full flex-col justify-end lg:justify-normal">
-        <div className="flex w-full items-center justify-center rounded-t-3xl bg-stone-200 py-8 lg:bg-inherit">
+        <div className="fixed bottom-0 md:static flex w-full items-center justify-center rounded-t-3xl bg-stone-200 py-8 lg:bg-inherit">
           <Link
             to="/posts"
             className="rounded-2xl bg-indigo-600 text-indigo-50 w-4/5 px-5 py-4 text-center font-fontBody text-xl uppercase flex items-center justify-between focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 focus:outline-none active:scale-90 lg:font-medium lg:mt-6"
